991a7af21c011b472540283af57cd10831b98abf
morralex <morr.alex@outlook.com>
2019-06-09 15:42:04 -0700

    working on getting it to display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,67 @@
 //first lets build the alphabet array
 
-var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//create variables that will hold numbers of wins losses etc
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 7;
+var ltrGuessed = [];
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+
+var winsText = document.getElementById("wins");
+var lossText = document.getElementById("losses");
+var guessesLeftText = document.getElementById("left");
+var userChoiceText = document.getElementById("guesses");
 
 //next lets pull a random character from the array for a computer guess
 
-var comGuess = alpha[Math.floor(Math.random()*alpha.length)];
+var comGuess = alpha[Math.floor(Math.random() * alpha.length)];
 
-console.log(comGuess)
+console.log(comGuess);
 
-//we need a way for the user to interact with the game
+//we need a function that is ran when the user makes a guess
 
-document.getElementById("guesses").onkeyup = function() {myFunction()};
+document.onkeyup = function (event) {
 
-function myFunction() {
-  var x = document.getElementById("guesses");
+    var userGuess = event.key;
+    ltrGuessed.push(userGuess);
+    console.log(userGuess)
+
+    if ((userGuess === comGuess) ) {
+        wins++;
+        winsText.innerHTML = "Wins: " + wins;
+    } else if ((userGuess !== comGuess[0]) && (guessesLeft > 0)) {
+        guessesLeft = guessesLeft - 1;
+    }
+    else {
+        losses++;
+
+        //Displaying the changing var's
+
+        
+    }
+         
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
